eb7c1570a1b8eed8c39366b938398385d22c10c6
Ridmi-Manarandi <ridmimanarandi@gmail.com>
2023-11-30 09:43:09 +0530

    add more comments

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-// server.js
-
+//required dependencies
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,13 +6,15 @@ const sql = require('mssql/msnodesqlv8');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
+
+//check the JWT_SECRET key succesfully added or not
 console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
 
 const app = express();
 const port = 3000;
 
-// MSSQL configuration
+// MSSQL configuration(windows authentication)
 const config = {
   driver: 'msnodesqlv8',
   connectionString: 'Driver={SQL Server Native Client 11.0};Server={ITELPTD083\\SQLEXPRESS02};Database={testdb};Trusted_Connection=yes;',
@@ -26,11 +27,11 @@ sql.connect(config, err => {
 });
 
 app.use(bodyParser.json());
+//user signup
 app.post('/register', async (req, res) => {
     try {
       const { username, email, password } = req.body;
   
-      // Perform validation, e.g., check if username or email already exists
   
       // Insert user into the database
       const result = await sql.query(`INSERT INTO Users (USERNAME, EMAIL, USERPASSWORD) VALUES ('${username}', '${email}', CONVERT(VARBINARY(64), HASHBYTES('SHA2_256', '${password}')))`);
@@ -46,15 +47,16 @@ app.post('/register', async (req, res) => {
 async function comparePasswords(enteredPassword, storedPassword) {
   return await bcrypt.compare(enteredPassword, storedPassword);
 }
-
-const jwtSecret = process.env.JWT_SECRET || 'default_secret_key';
+//set default secret key if the key is not available
+const jwtSecret = process.env.JWT_SECRET || '123ridmi';
+//user login
 app.post('/login', async (req, res) => {
     try {
       const { username, password } = req.body;
   
       // Retrieve user from the database
       const result = await sql.query`SELECT * FROM Users WHERE USERNAME = ${username}`;
-  
+      //check the user is exist
       if (result.recordset.length === 0) {
         res.status(401).json({ success: false, message: 'Invalid username or password' });
       } else {
@@ -82,10 +84,12 @@ app.post('/login', async (req, res) => {
       res.status(500).json({ success: false, message: 'Internal Server Error' });
     }
   });
+  //get all users
   app.get('/users', async (req, res) => {
     try {
       // Retrieve all users from the database
       const result = await sql.query`SELECT * FROM Users`;
+      //print the all users details in console
       console.log(result.recordset);
   
       // Send the list of users in the response
@@ -95,6 +99,8 @@ app.post('/login', async (req, res) => {
       res.status(500).json({ success: false, message: 'Internal Server Error' });
     }
   });
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
